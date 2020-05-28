48bc31a6905414033e524c0dbe5b637e25deb21c
Abhinav Nagaraj <nagaraj.ab@northeastern.edu>
2020-05-28 00:41:27 -0400

    test

--- models/user.js
+++ models/user.js
@@ -1,25 +1,25 @@
+'use strict';
+var bcrypt = require('bcrypt-nodejs');
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      username: DataTypes.STRING,
-      password: DataTypes.STRING,
-      FirstName: DataTypes.STRING,
-      LastName: DataTypes.STRING
-    }, {});
-    User.beforeSave((user, options) => {
-      if (User.changed('password')) {
-        User.password = bcrypt.hashSync(User.password, bcrypt.genSaltSync(10), null);
-      }
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  });
+  User.beforeSave((user, options) => {
+    if (user.changed('password')) {
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    }
+  });
+  User.prototype.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
     });
-    User.prototype.comparePassword = function (passw, cb) {
-      bcrypt.compare(passw, this.password, function (err, isMatch) {
-          if (err) {
-              return cb(err);
-          }
-          cb(null, isMatch);
-      });
-    };
-    User.associate = function(models) {
-      // associations can be defined here
-    };
-    return User;
   };
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
